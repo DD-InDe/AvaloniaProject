81ca13e6062288cd73b59a13a3d8c1bd0eb38cb9
deer <bogdanovich_7712@mail.ru>
2024-12-24 12:38:42 +0300

    Initial commit

---
+++ Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDApp.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Lastname { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Middlename { get; set; } = null!;
+
+    public byte[]? Photo { get; set; }
+
+    public int? Role { get; set; }
+
+    public virtual Role? RoleNavigation { get; set; }
+}
